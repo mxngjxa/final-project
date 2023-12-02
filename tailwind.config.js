7dbb1d9187dcb33a35e3ce1dfb6bab4e1462d002
M. G. <mingjia.guan@gmail.com>
2023-12-02 06:25:10 +0000

    feat: updated redirect url

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,15 @@ module.exports = {
   theme: {
     extend: {
       backgroundColor: {
-        'sky-blue': '#87CEEB',
-        'light-spring-green': '#00FA9A',
-        'pastel-light-green': '#C8FFB0',
+        "sky-blue": "#87CEEB",
+        "light-spring-green": "#00FA9A",
+        "pastel-light-green": "#C8FFB0",
       },
       textColor: {
-        'gray-600': '#4B5563',   // Adjusted gray
-        'indigo-700': '#5A67D8', // Adjusted indigo
-        'green-600': '#38A169',  // Adjusted green
-        'blue-600': '#3B82F6',   // Adjusted blue
+        "gray-600": "#4B5563", // Adjusted gray
+        "indigo-700": "#5A67D8", // Adjusted indigo
+        "green-600": "#38A169", // Adjusted green
+        "blue-600": "#3B82F6", // Adjusted blue
       },
     },
   },
